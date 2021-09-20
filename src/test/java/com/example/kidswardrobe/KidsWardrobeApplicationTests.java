ee8ab4e448ecabdfd2cb2d81bcf4078e059381d3
EllenEnnok <ellenike@hotmail.com>
2021-09-20 04:00:58 -0700

    initial commit

---
+++ src/test/java/com/example/kidswardrobe/KidsWardrobeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kidswardrobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsWardrobeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
