460149349bbf985fc377a19e2e51d5cc7d091621
SigneU <signe.unt@gmail.com>
2021-09-30 09:56:30 +0300

    uus vue põhi Signe

--- src/client-app/src/App.vue
+++ src/client-app/src/App.vue
@@ -1,25 +1,29 @@
 <template>
-  <div id="app" class="container">
-   <Header></Header>
-<div id="nav">
-      <router-link to="/">
-        <img src="home.svg">Avaleht</router-link> |
-      <router-link to="/riidekapp"><svg class="icon">
-        <use xlink:href="#clothes" />
-      </svg>
-        Riidekapp
-      </router-link>       |
-      <router-link to="/filtreeri">Filtreeri</router-link>
-       |
-      <router-link to="/otsi">
-        <input class="form-control mr-sm-2" type="search" placeholder="Otsi" aria-label="Otsi">
-        <button>Otsi</button>
-      </router-link>
-       |
-      <router-link to="/peamenuu"><i class="fa fa-bars"></i></router-link>
+  <div class="app">
+<!--    <div class="container">-->
+      <Header></Header>
+      <div class="nav">
+        <router-link to="/">
+          <img src="home.svg">Avaleht
+        </router-link>
+        |
+        <router-link to="/riidekapp">
+          <img src="public/wardrobe.svg">
+          Riidekapp
+        </router-link>
+        |
+        <router-link to="/filtreeri"><img src="public/magnifying-glass.svg">Filtreeri</router-link>
+        |
+        <router-link to="/otsi">
+          <input class="form-control mr-sm-2" type="search" placeholder="Otsi" aria-label="Otsi">
+          <button><img src="public/magnifying-glass-1.svg">Otsi</button>
+        </router-link>
+        |
+        <router-link to="/peamenuu"><i class="fa fa-bars"></i></router-link>
 
-    <router-view/>
-  </div>
+        <router-view/>
+      </div>
+<!--    </div>-->
   </div>
 </template>
 <script>
@@ -32,6 +36,7 @@ export default {
 
   }
 }
+
 </script>
 
 <style>
