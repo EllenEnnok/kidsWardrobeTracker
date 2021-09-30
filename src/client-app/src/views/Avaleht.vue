fc0fde26de77002e02e40b6654e8fe12588eba29
Styna <styna.telliskivi@outlook.com>
2021-09-30 16:22:19 +0300

    Põhi uueneb

--- src/client-app/src/views/Avaleht.vue
+++ src/client-app/src/views/Avaleht.vue
@@ -1,7 +1,19 @@
 <template>
 
-  <div class="">
-    </div>
+  <div class="avaleht">
+    <label>E-post</label>
+    <input type="text" v-model="kasutajanimi"/>
+    <br>
+    <label>Salasõna</label>
+    <input type="password" v-model="parool"/>
+    <br>
+    <button v-on:click="login">Logi sisse</button>
+
+    <h1>Pole kontot?</h1>
+    <GoTo title="Loo uus konto" link="/uuskonto"></GoTo>
+
+    <button v-on:click="logout">Logi välja</button>
+  </div>
 </template>
 
 <script>
